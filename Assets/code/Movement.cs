f7974d2ddd4d2e4bd624304e7d6a11ff4977b7bd
Isaiah Brown <86494264+Matt093@users.noreply.github.com>
2024-09-10 21:39:26 -0400

    Whale Game

---
+++ Assets/code/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    private Vector2 _movement;
+
+    public Collision2D collision;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+        _movement = new Vector2(_movement.x, _movement.y).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+}
